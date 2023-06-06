1987cec6228740b8a48fc0f92e7daed1789d387b
Mateus Sanches Rodriguez <mateussanchesrodriguez@gmail.com>
2023-06-06 13:44:14 -0300

    projeto configurado com Redux

--- src/containers/Produtos.tsx
+++ src/containers/Produtos.tsx
@@ -1,21 +1,15 @@
 import { Produto as ProdutoType } from '../App'
+import { useSelector } from 'react-redux'
 import Produto from '../components/Produto'
+import { useGetProdutosQuery } from '../services/api'
+
+import { RootReducer } from '../store'
 
 import * as S from './styles'
 
-type Props = {
-  produtos: ProdutoType[]
-  favoritos: ProdutoType[]
-  adicionarAoCarrinho: (produto: ProdutoType) => void
-  favoritar: (produto: ProdutoType) => void
-}
+const ProdutosComponent = () => {
+  const favoritos = useSelector((state: RootReducer) => state.favotiro.itens)
 
-const ProdutosComponent = ({
-  produtos,
-  favoritos,
-  adicionarAoCarrinho,
-  favoritar
-}: Props) => {
   const produtoEstaNosFavoritos = (produto: ProdutoType) => {
     const produtoId = produto.id
     const IdsDosFavoritos = favoritos.map((f) => f.id)
@@ -23,16 +17,20 @@ const ProdutosComponent = ({
     return IdsDosFavoritos.includes(produtoId)
   }
 
+  const { data: produtos, isLoading } = useGetProdutosQuery()
+
+  if (isLoading) {
+    return <h2>Carregando...</h2>
+  }
+
   return (
     <>
       <S.Produtos>
-        {produtos.map((produto) => (
+        {produtos?.map((produto) => (
           <Produto
             estaNosFavoritos={produtoEstaNosFavoritos(produto)}
             key={produto.id}
             produto={produto}
-            favoritar={favoritar}
-            aoComprar={adicionarAoCarrinho}
           />
         ))}
       </S.Produtos>
